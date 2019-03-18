6c95833d57e1a6534cbdbf13e3864d2fe3ea7035
s267678 <47784082+s267678@users.noreply.github.com>
2019-03-18 09:25:38 -0400

    Added NumericTextField

---
+++ NumericTextFieldsIsabelleLC/main.lua
@@ -0,0 +1,13 @@
+-- Title: NumericTextFields
+-- Name: Isabelle LC
+-- Course: ICS2O/3C
+-- This program displays a math question and asks the user to answer in a numeric
+-- textfield terminal.
+------------------------------------------------------------------------------------------------
+
+-- hide the status bar
+display.setStatusBar(display.HiddenStatusBar)
+
+-- sets the background colour
+display.setDefault("background", 255/255, 180/255, 80/255)
+
